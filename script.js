d8a142b601e631da34e37ea228655147a265a9bb
weconvest <adhamohamed82@gmail.com>
2026-05-06 14:32:29 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+document.getElementById("name").addEventListener("input", function() {
+    document.getElementById("output").innerText = "Hello " + this.value;
+});
+
+document.getElementById("btn").addEventListener("click", function() {
+    document.getElementById("clickText").innerText = "Success!";
+});
+
+document.getElementById("box").addEventListener("mouseover", function() {
+    this.innerText = "The faculty is located in Zamalek, Egypt.";
+});
+
+document.getElementById("box").addEventListener("mouseout", function() {
+    this.innerText = "Hover over me for location";
+});
